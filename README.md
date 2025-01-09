2d9718bbf5671de9e3334282de61928bf5cae739
Christian Dinkel <christian.dinkel@omicronenergy.com>
2025-01-10 00:44:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## OpenSCD Diff plugin
+
+This plugin allows to compare two SCL files and display the differences in a tree view.
+
+&copy; 2025 OMICRON electronics GmbH
---
+++ rollup.config.js
@@ -0,0 +1,61 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
+import esbuild from 'rollup-plugin-esbuild';
+import { generateSW } from 'rollup-plugin-workbox';
+import path from 'path';
+
+export default {
+  input: 'oscd-diff.ts',
+  output: {
+    format: 'es',
+    dir: 'dist',
+  },
+  preserveEntrySignatures: true,
+
+  plugins: [
+    /** Resolve bare module imports */
+    nodeResolve(),
+    /** Minify JS, compile JS to a lower language target */
+    esbuild({
+      minify: true,
+      target: ['esnext'],
+    }),
+    /** Bundle assets references via import.meta.url */
+    importMetaAssets(),
+    /** Minify html and css tagged template literals */
+    babel({
+      plugins: [
+        [
+          'babel-plugin-template-html-minifier',
+          {
+            modules: { lit: ['html', { name: 'css', encapsulation: 'style' }] },
+            failOnError: false,
+            strictCSS: true,
+            htmlMinifier: {
+              collapseWhitespace: true,
+              conservativeCollapse: true,
+              removeComments: true,
+              caseSensitive: true,
+              minifyCSS: true,
+            },
+          },
+        ],
+      ],
+    }),
+    /** Create and inject a service worker */
+    generateSW({
+      globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
+      navigateFallback: '/index.html',
+      // where to output the generated sw
+      swDest: path.join('dist', 'sw.js'),
+      // directory to match patterns against to be precached
+      globDirectory: path.join('dist'),
+      // cache any html js and css by default
+      globPatterns: ['**/*.{html,js,css,webmanifest}'],
+      skipWaiting: true,
+      clientsClaim: true,
+      runtimeCaching: [{ urlPattern: 'polyfills/*.js', handler: 'CacheFirst' }],
+    }),
+  ],
+};
