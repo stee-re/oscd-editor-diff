8c69beef350a08a4c9dc53a8a54866a25fa86cc5
cad <christian.dinkel@omicronenergy.com>
2025-03-03 08:37:21 +0100

    feat: explicitly label text content as such (#50)

--- hash.ts
+++ hash.ts
@@ -876,7 +876,7 @@ export function hasher(
     }
     const textContent = e.textContent?.trim();
     if (textContent) {
-      description['>'] = textContent;
+      description['Text Content'] = textContent;
     }
     return description;
   }
